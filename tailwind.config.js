403b211f6711b3092682a8c341b6d60c2fd36c8d
Shiv-468 <shivmuwal.147@gmail.com>
2023-12-30 13:02:27 +0530

    _add css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins','sans-serif'],
+        'roboto':['Roboto', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
